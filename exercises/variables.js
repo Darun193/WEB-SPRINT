9bc1af2ea75a00077cb3d9fa36d054c3ffe58848
dmaximenko <darya@bravora.dev>
2026-08-06 00:58:09 +0500

    Добавлены результаты упражнения модуля 0: скрипт для сортировки

---
+++ exercises/variables.js
@@ -0,0 +1,47 @@
+const userName = "Darya";
+const age = 20;
+
+console.log(userName);
+console.log(age);
+
+let completedLessons = 1;
+console.log(completedLessons);
+
+completedLessons = 2;
+console.log(completedLessons);
+
+const city = "Astana";
+const confidence = 0.97;
+const isReviewed = false;
+let result;
+
+console.log(typeof city);
+console.log(typeof confidence);
+console.log(typeof isReviewed);
+console.log(typeof result);
+
+// практическое задание
+
+const courseName = "Web-sprint";
+const currentModule = 3;
+const isComleted = false;
+let mentorFeedback;
+
+console.log(typeof courseName);
+console.log(typeof currentModule);
+console.log(typeof isComleted);
+console.log(typeof mentorFeedback);
+
+// математические операторы 
+
+const hourlyRate = 2500;
+const workedHours = 4;
+const earnings = hourlyRate * workedHours;
+
+console.log(earnings);
+
+const people = 3;
+const amountPerPerson = earnings/people;
+
+console.log(amountPerPerson);
+console.log(10%3);
